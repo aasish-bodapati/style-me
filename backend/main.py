ac1c01fc6c17111475908c70bbd244d2165041b9
Aasish Bodapati <95697158+aasish-bodapati@users.noreply.github.com>
2026-03-29 15:38:37 +0530

    Initital Commit- Setup venv, gitignore, env and main.py.

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,14 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
-
-class Item(BaseModel):
-    name: str
-    category: str | None = None
 
 
-app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # allow all (for now)
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-@app.get("/")
+
+
+app= FastAPI()
+
+
+
+@app.get("/health")
 def home():
-    return {"message": "Style me Running"}
-
-@app.post("/add-item")
-def add_item(item: Item):
-    return {"message": f"Item {item.name} added successfully!"}
+    return "Status: Stylist is awake!"
